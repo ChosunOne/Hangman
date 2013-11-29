5660a4941a4a5af7e729ce57cc48c26d2ed10de9
jbreedlove <jrbrrf@mail.missouri.edu>
2013-11-29 17:22:03 -0600

    Added Intro
    Intro to the Program

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -9,6 +9,34 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
+            //Declarations
+            string pause;
+            string check;
+
+            //Intro
+            Console.Write("________Hangman_______\n\n");
+            Console.Write("Developed by:\n");
+            Console.Write("Josiah Evans and Jonathan Breedlove\n");
+
+            //Input
+            Console.Write("\n\nStart a new game?(yes/no):");
+            check = Console.ReadLine();
+
+            //Check user's input
+            if( check == "yes" )
+            {
+                Console.Write("\n\nInput was yes\n\n");
+            }
+            else if (check == "no")
+            {
+                Console.Write("\n\nInput was no\n\n");
+            }
+            else
+            {
+                Console.Write("\n\nThat was not valid input\n\n");
+            }
+            
+            pause = Console.ReadLine();
         }
     }
 }
