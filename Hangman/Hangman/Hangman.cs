93eed2907012c57b1838513aff0b84646133a28a
jbreedlove <jrbrrf@mail.missouri.edu>
2013-11-29 18:39:33 -0600

    Added Word Checker
    FillInBlanks
    CheckLetter
    FindIndices

--- Hangman/Hangman/Hangman.cs
+++ Hangman/Hangman/Hangman.cs
@@ -9,7 +9,7 @@ namespace Hangman
     {
         string word;
         string guessed;
-
+        string letter;
 
         public Hangman()
         {
@@ -24,6 +24,67 @@ namespace Hangman
                 Console.Write("_ ");
             }
             Console.Write("\n");
+        }
+
+        public void GuessLetter()
+        {
+            Console.Write("Please enter a letter: ");
+            letter = Console.ReadLine();
+        }
+
+        public bool CheckLetter( string l )
+        {
+            char guess = l[0];
+
+            foreach (var i in word)
+            {
+                if(i == guess)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public List<int> FindIndices( string w, string l )
+        {
+            int count = 0;
+            List<int> indexAry;
+            indexAry = new List<int>();
+
+            foreach( var i in word )
+            {
+                if( i == l[0] )
+                {
+                    indexAry.Add(count);
+                }
+
+                count++;
+            }
+
+            return indexAry;
+        }
+
+        public void FillInBlanks( List<int> indexList, string l )
+        {
+            char[] replace;
+            int length = guessed.Length;
+            replace = new char[length];
+            int count = 0;
+
+            foreach(var j in guessed)
+            {
+                replace[j] = guessed[count];
+                count++;
+            }
+
+            foreach (var i in indexList)
+            {
+                replace[i] = l[0];
+            }
+
+            guessed = replace.ToString();
         }
 
         public string Word
